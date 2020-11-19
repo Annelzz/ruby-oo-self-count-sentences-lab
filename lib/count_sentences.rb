162505d7e84e1d7953b5032b9a25428b2cc30c3e
Annelise <annelzz26@gmail.com>
2020-11-18 16:59:48 -0800

    commit tree 3e25bfde204d5976387eeefeeff665979ed4c7fa

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sections = self.split(/\.|\?|\!/)
+    sections.delete_if do |part|
+      part.size < 2
+    end
+    .size
   end
 end
